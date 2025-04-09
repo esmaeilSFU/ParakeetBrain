25850bba5b6870fd9026b8bca8979d68b12ec722
Esmaeil Mottaghi <motaghi.esmaeil@gmail.com>
2025-04-09 16:49:43 -0700

    the gh plugin is created

---
+++ ParakeetBrain/ParakeetBrainInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace ParakeetBrain
+{
+    public class ParakeetBrainInfo : GH_AssemblyInfo
+    {
+        public override string Name => "ParakeetBrain";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("45762455-0076-473f-bfbf-ea786cbf3c52");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
